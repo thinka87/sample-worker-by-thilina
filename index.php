484e1e00ccfafbe6670235feaeed86d212652e1d
thinka87 <thinkaemoney@gmail.com>
2022-01-25 04:45:27 +0530

    error reporting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL |E_STRICT);
+ini_set("display_errors", 1);
+
 //Use SPL auto load SPL register instead of composer
 spl_autoload_register(function ($class) {
     include ( realpath(dirname(__FILE__)) . '/src/'.$class.'.php' );
